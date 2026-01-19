b9fb4462669bd8d6a9bc5418c886f069b5236f02
zzmmxxx <zzmmxxx@github.com>
2026-01-19 15:45:40 +0800

    Initial commit: Original mutant for astropy modeling separable issue

--- astropy/modeling/separable.py
+++ astropy/modeling/separable.py
@@ -242,7 +242,7 @@ def _cstack(left, right):
         cright = _coord_matrix(right, 'right', noutp)
     else:
         cright = np.zeros((noutp, right.shape[1]))
-        cright[-right.shape[0]:, -right.shape[1]:] =  1 if right.shape[0] == right.shape[1] else 0 
+        cright[-right.shape[0]:, -right.shape[1]:] = 1
     return np.hstack([cleft, cright])
 
 
